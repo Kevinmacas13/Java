25d79d4afbae87628be8c5e7513c24589dc1ef67
kevin <kevinmacash@gmail.com>
2025-11-04 10:44:25 -0500

    feat: adding Classes project

---
+++ Clases/src/com/krakedev/TestPersona.java
@@ -0,0 +1,21 @@
+package com.krakedev;
+
+public class TestPersona {
+
+	public static void main(String[] args) {
+		Persona p;
+
+        Persona p2;
+        p2= new Persona();
+        p2.nombre="Vicky";
+		p = new Persona();
+        p.nombre = "Kevin";
+        p.edad = 20;
+        p.edad=15;
+		System.out.println("Mi persona"+ p.nombre);
+        System.out.println("Eres mi vida "+p.nombre);
+
+       
+	}
+
+}
