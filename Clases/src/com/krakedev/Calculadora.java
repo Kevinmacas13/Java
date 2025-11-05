ad0733cfd4f842833e5fa799b83dedf721b16d41
kevin <kevinmacash@gmail.com>
2025-11-05 18:48:03 -0500

    feat: adding dividir  method in Calculadora

--- Clases/src/com/krakedev/Calculadora.java
+++ Clases/src/com/krakedev/Calculadora.java
@@ -17,20 +17,13 @@ public class Calculadora {
         resultado = valor1 * valor2;
         return resultado;
     }
-
     public  double dividir(double valor1, double valor2){
         double resultado;
         resultado = valor1 / valor2;
         return resultado;
     }
-    public  double promediar(double valor1, double valor2, double valor3){
-        double resultado;
-        resultado = (valor1 + valor2 + valor3)/3;
-        return resultado;
-    }
-    public  void mostrarResultado(){
-       System.out.println("Ahorita no joven, Salí al almuerzo .Regreso en 15 minutos.");
-    }
+
+
 
 
 
