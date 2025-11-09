c2881d7ad5081b7087d7755734daf12b1764bf6c
kevin <kevinmacash@gmail.com>
2025-11-09 08:36:31 -0500

    feat: completed exercise 1   in Proyecto Cartas

---
+++ ProyectoCartas/src/com/krakedev/entidades/Carta.java
@@ -0,0 +1,30 @@
+package com.krakedev.entidades;
+
+public class Carta {
+    private int numero;
+    private String palo;
+
+    public Carta(int numero, String palo) {
+        this.numero = numero;
+        this.palo = palo;
+    }
+    public void mostrarInformacion(){
+        System.out.println(this.numero + "-" + this.palo);
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getPalo() {
+        return palo;
+    }
+
+    public void setPalo(String palo) {
+        this.palo = palo;
+    }
+}
---
+++ ProyectoCartas/src/com/krakedev/entidades/Palos.java
@@ -0,0 +1,45 @@
+package com.krakedev.entidades;
+
+public class Palos {
+    private String corazonRojo;
+    private String corazonNegro;
+    private String diamante;
+    private String trebol;
+
+    public Palos(){
+
+    }
+
+
+    public String getCorazonRojo() {
+        return corazonRojo;
+    }
+
+    public void setCorazonRojo(String corazonRojo) {
+        this.corazonRojo = corazonRojo;
+    }
+
+    public String getCorazonNegro() {
+        return corazonNegro;
+    }
+
+    public void setCorazonNegro(String corazonNegro) {
+        this.corazonNegro = corazonNegro;
+    }
+
+    public String getDiamante() {
+        return diamante;
+    }
+
+    public void setDiamante(String diamante) {
+        this.diamante = diamante;
+    }
+
+    public String getTrebol() {
+        return trebol;
+    }
+
+    public void setTrebol(String trebol) {
+        this.trebol = trebol;
+    }
+}
